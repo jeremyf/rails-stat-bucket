745be864677a83705c74f95b4967699337d49d3d
Jeremy Friesen <jeremyf@lightsky.com>
2009-04-06 21:17:59 -0400

    Begin the process of parsing the git history.

--- lib/rails-stat-bucket.rb
+++ lib/rails-stat-bucket.rb
@@ -1,9 +1,30 @@
 module RailsStatBucket
-  def self.generate(params)
-
+  def self.generate(input_directory, output_filename, verbose = false)
+    git = `which git`.strip
+    if system("cd #{input_directory} && #{git} status | grep 'nothing to commit' > /dev/null")
+      puts 'Nothing to commit, proceeding with stats check.'
+      
+      previous_date = nil
+      %x{cd #{input_directory} && #{git} log --date=short | grep 'Date:' -B 2}.split('--').each do |log_entry|
+        if matched_data = log_entry.match(/^Date: +(\d{4}-\d{2}-\d{2})/)
+          current_date = matched_data[1]
+          if verbose || previous_date != current_date 
+            puts current_date
+          end
+          previous_date = current_date
+        end
+      end
+    else
+      puts 'Unable to proceed with stat check. Directory directory.'
+      exit!(-1)
+    end
   end
 
   def self.load(params)
+
+  end
+
+  class Entry
 
   end
 end
